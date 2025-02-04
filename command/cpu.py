267f5157b296fff9deeea19afc0684afba7eb208
Mahdi <mahdi@Mahdis-MacBook-Pro.local>
2025-02-04 14:41:10 +0330

    comments added, error code variable added

--- command/cpu.py
+++ command/cpu.py
@@ -7,15 +7,18 @@ from easycli import SubCommand, Argument
 from utilities.response.command.cpu import CpuUsageResponse
 
 
+# cpu usage subcommand
 class Cpu(SubCommand):
     __command__ = 'cpu'
     __arguments__ = [
+        # argument for showing usage in intervals
         Argument(
             '-i', '--interval',
             action='store',
             type=float,
             help='Interval in seconds'
         ),
+        # for showing usage usage per cpu core
         Argument(
             '-pc', '--per-core',
             action='store_true',
@@ -26,23 +29,21 @@ class Cpu(SubCommand):
     def __call__(self, args):
         interval = args.interval
         per_core = args.per_core
-
+        
         try:
-            while True:
-                cpu_usage = psutil.cpu_percent(interval=interval, percpu=per_core)
-                data = {"cpu_usage": cpu_usage, "per_core": per_core}
-                CpuUsageResponse.success(data=data)
+            while True: # handling interval
+                cpu_usage = psutil.cpu_percent(interval=interval, percpu=per_core) # handle per core argument inline
+                data = {"cpu_usage": cpu_usage, "per_core": per_core} # parse data for response
+                CpuUsageResponse.success(data=data) # calling response class success method
 
                 if not interval:
                     sys.exit(0)
                 else:
                     time.sleep(interval)
 
-        except KeyboardInterrupt:
-            message = " Monitoring Stoped"
-            CpuUsageResponse.error(message=message)
-            sys.exit(0)
+        except KeyboardInterrupt: # kill signal (ctrl+C) handling
+            message = ''
+            CpuUsageResponse.error(message=message, code=0) # calling response class error method
 
         except Exception as e:
             CpuUsageResponse.error(message=e)
-            sys.exit(0)
