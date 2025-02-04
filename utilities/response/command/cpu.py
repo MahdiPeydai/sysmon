a6f851c5b80e77c5eb6df6b39e422555528d492d
Mahdi <mahdi@Mahdis-MacBook-Pro.local>
2025-02-04 14:39:15 +0330

    comments added, per core argument outut changed

--- utilities/response/command/cpu.py
+++ utilities/response/command/cpu.py
@@ -9,8 +9,10 @@ class CpuUsageResponse(Response):
     def success(data):
         cpu_usage = data['cpu_usage']
 
+        # argumnet -pc for showing usage for per core
         if data['per_core']:
             for i, usage in enumerate(cpu_usage):
-                print(f"Core {i} : {usage}%")
+                print(f"Core {i} usage : {usage}%")
+        # pure command for showing system cpu usage
         else:
             print(f"CPU Usage : {cpu_usage}%")
