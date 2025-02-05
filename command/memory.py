9f37af05eafc67d3cb3e783e6b23ee71c006fe2d
MahdPeydai <mahdipeydai@gmail.com>
2025-02-05 16:02:32 +0330

    using Mutex, adding code variable to response error method, adding comments

--- command/memory.py
+++ command/memory.py
@@ -2,29 +2,36 @@ import psutil
 import time
 import sys
 
-from easycli import SubCommand, Argument
+from easycli import SubCommand, Argument, Mutex
 
 from utilities.response.command.memory import MemoryUsageResponse
 
 
+# memory usage monitoring command
 class Memory(SubCommand):
     __command__ = 'memory'
     __arguments__ = [
+        # argument for monitoring usage in intervals
         Argument(
             '-i', '--interval',
             action='store',
             type=float,
             help='Interval in seconds'
         ),
-        Argument(
-            '-a', '--advance',
-            action='store_true',
-            help='Memory usage advance mode'
-        ),
-        Argument(
-            '-s', '--swap',
-            action='store_true',
-            help='Swap memory usage'
+        # defining mutex arguments
+        Mutex(
+            # argument for monitoring memory in advance mode
+            Argument(
+                '-a', '--advance',
+                action='store_true',
+                help='Memory usage advance mode'
+            ),
+            # argument for monitoring swap memory
+            Argument(
+                '-s', '--swap',
+                action='store_true',
+                help='Swap memory usage'
+            )
         )
     ]
 
@@ -32,36 +39,36 @@ class Memory(SubCommand):
         swap = args.swap
         advance = args.advance
         interval = args.interval
+        if interval and interval < 0: # validating interval argument
+            message = "Error: Interval must be a positive integer!"
+            MemoryUsageResponse.error(message=message, code=2)
 
         try:
-            if swap:
-                if advance or interval:
-                    message = "Error: Invalid arguments for swap monitoring"
-                    MemoryUsageResponse.error(message=message)
-                else:
-                    swap = psutil.swap_memory()
-                    data = {'swap': swap}
-                    MemoryUsageResponse.success(data=data)
-                    sys.exit(0)
-            else:
-                if interval and interval < 0:
-                    message = "Error: Interval must be a positive integer!"
-                    MemoryUsageResponse.error(message=message)
+            if swap: # handling swap usage monitoring
+                while True: # handling interval
+                    swap = psutil.swap_memory() # getting swap memory
+                    data = {'swap': swap} # composing data for response
+                    MemoryUsageResponse.success(data=data) # calling response class success method
 
+                    if not interval:
+                        sys.exit(0)
+                    else:
+                        time.sleep(interval)
+
+            else: # handling memory usage monitoring
                 while True:
-                    memory = psutil.virtual_memory()
-                    data = {'memory':memory, 'advance':advance}
+                    memory = psutil.virtual_memory() # geeting memory usage
+                    data = {'memory':memory, 'advance':advance} # handling advance argument
                     MemoryUsageResponse.success(data=data)
 
                     if not interval:
                         sys.exit(0)
                     else:
                         time.sleep(interval)
-        except KeyboardInterrupt:
-            message = " Monitoring Stoped"
-            MemoryUsageResponse.error(message=message)
-            sys.exit(0)
+
+        except KeyboardInterrupt: 
+            message = ''
+            CpuUsageResponse.error(message=message, code=0) # calling response class error method
 
         except Exception as e:
-            MemoryUsageResponse.error(message=e)
-            sys.exit(0)
+            CpuUsageResponse.error(message=e)
